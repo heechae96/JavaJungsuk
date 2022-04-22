b6bbf00ab7987c59745a70b2a3abf013176e2bed
heechae96 <shinheechae@naver.com>
2022-04-23 00:32:09 +0900

    변수의 초기화

---
+++ javajungsuk/src/ch02/Ch2_1.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class Ch2_1 {
+
+	public static void main(String[] args) {
+
+		// 변수의 초기화
+		
+		int year = 0;
+		int age = 14;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		year = age + 2000;
+		age = age +1;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+	}
+
+}
