048ca13d3ffbc441f6edbb28c0319b68519d2a9d
heechae96 <shinheechae@naver.com>
2022-04-27 22:45:58 +0900

    형변환의 방법

---
+++ javajungsuk/src/ch02/Ch2_12.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class Ch2_12 {
+
+	public static void main(String[] args) {
+
+		// 형변환의 방법
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score="+score);
+		System.out.println("d="+d);
+	}
+
+}
