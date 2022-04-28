f73c5a3d46c71e8995484d254fdff4227cd3fcbe
heechae96 <shinheechae@naver.com>
2022-04-28 12:28:41 +0900

    증감 연산자

---
+++ javajungsuk/src/ch03/Ch3_1.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class Ch3_1 {
+
+	public static void main(String[] args) {
+		
+		// 증감 연산자
+		int i = 5;
+		i++;
+		System.out.println(i);
+		
+		i = 5;
+		++i;
+		System.out.println(i);
+	}
+}
