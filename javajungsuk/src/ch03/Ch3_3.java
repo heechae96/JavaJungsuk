57b141f148e0443911db3502f987a12dac3f8107
heechae96 <shinheechae@naver.com>
2022-04-28 12:38:23 +0900

    증감 연산자(메서드 호출에 사용된 예)

---
+++ javajungsuk/src/ch03/Ch3_3.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class Ch3_3 {
+
+	public static void main(String[] args) {
+
+		// 증감 연산자(메서드 호출에 사용된 예)
+		int i = 5, j = 5;
+		System.out.println(i++);
+		System.out.println(++j);
+		System.out.println("i = "+i+", j = "+j);
+	}
+
+}
