5c246075367669798634c1bfba35ad93a7017694
heechae96 <shinheechae@naver.com>
2022-04-28 12:47:30 +0900

    부호 연산자

---
+++ javajungsuk/src/ch03/Ch3_4.java
@@ -0,0 +1,18 @@
+package ch03;
+
+public class Ch3_4 {
+
+	public static void main(String[] args) {
+
+		// 부호 연산자
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);
+
+	}
+
+}
