3da03deb062485b98e5862eab0601b7d9489d58c
heechae96 <shinheechae@naver.com>
2022-05-05 00:18:00 +0900

    배열의 길이와 인덱스

---
+++ javajungsuk/src/ch05/Ch5_1.java
@@ -0,0 +1,31 @@
+package ch05;
+
+public class Ch5_1 {
+
+	public static void main(String[] args) {
+
+		// 배열의 길이와 인덱스
+		int[] score = new int[5];
+		int k = 1;
+		
+		score[0] = 50;
+		score[1] = 60;
+		score[k+1] = 70;
+		score[3] = 80;
+		score[4] = 90;
+		
+		int tmp = score[k+2] + score[4];
+		
+		for(int i=0; i<5; i++) {
+			System.out.printf("score[%d]:%d%n", i, score[i]);
+		}
+		
+		System.out.printf("tmp:%d%n",tmp);
+		
+		// 인덱스가 벗어난 값
+		//	-> java.lang.ArrayIndexOutOfBoundsException
+		System.out.printf("score[%d]:%d%n", 7, score[7]);
+		
+	}
+
+}
