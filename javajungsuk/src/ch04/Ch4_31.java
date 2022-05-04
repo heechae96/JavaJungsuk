7284a67a24e6efe854dad5e90b5da9c1cc783fbd
heechae96 <shinheechae@naver.com>
2022-05-04 12:43:42 +0900

    continue문

---
+++ javajungsuk/src/ch04/Ch4_31.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class Ch4_31 {
+
+	public static void main(String[] args) {
+
+		// continue문
+		for(int i=0; i<=10; i++) {
+			if(i%3==0)
+				continue;
+			System.out.println(i);
+		}
+	}
+
+}
