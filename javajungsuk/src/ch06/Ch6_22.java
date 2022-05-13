72e3e19209391a8921804a38537310b664ae84dc
heechae96 <shinheechae@naver.com>
2022-05-14 00:40:15 +0900

    가변인자와 오버로딩

---
+++ javajungsuk/src/ch06/Ch6_22.java
@@ -0,0 +1,33 @@
+package ch06;
+
+public class Ch6_22 {
+
+	public static void main(String[] args) {
+
+		// 가변인자와 오버로딩
+		String[] strArr = { "100", "200", "300" };
+		
+		System.out.println(concatenate("", "100", "200", "300"));
+		System.out.println(concatenate("-", strArr));
+		System.out.println(concatenate(",", new String[]{"1", "2", "3"}));
+		System.out.println("["+concatenate(",", new String[0])+"]");
+		System.out.println("["+concatenate(",")+"]");
+		
+	}
+
+	static String concatenate(String delim, String... args) {
+		String result = "";
+
+		for(String str : args) {
+			result += str + delim;
+		}
+		
+		return result;
+	}
+	
+	/*
+	static String concatenate(String ...args) {
+		return concatenate("", args);
+	}
+	*/
+}
