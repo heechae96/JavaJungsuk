a1678236df97f79f7570a3ce05308d632be8b2b7
heechae96 <shinheechae@naver.com>
2022-05-14 23:25:26 +0900

    기본 생성자

---
+++ javajungsuk/src/ch06/Ch6_23.java
@@ -0,0 +1,23 @@
+package ch06;
+
+class Data11{
+	int value;
+}
+
+class Data22{
+	int value;
+	
+	Data22(int x){
+		value = x;
+	}
+}
+public class Ch6_23 {
+
+	public static void main(String[] args) {
+
+		// 기본 생성자
+		Data11 d1 = new Data11();
+//		Data22 d2 = new Data22();	// 컴파일 에러 발생
+	}
+
+}
