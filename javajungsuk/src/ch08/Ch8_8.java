c3c0f61c9fb34ff394679bdee547bd97748a6c19
heechae96 <shinheechae@naver.com>
2022-05-27 01:17:08 +0900

    printStackTrace() 와 getMessage()

---
+++ javajungsuk/src/ch08/Ch8_8.java
@@ -0,0 +1,21 @@
+package ch08;
+
+public class Ch8_8 {
+
+	public static void main(String[] args) {
+
+		// printStackTrace() 와 getMessage()
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		}catch(ArithmeticException ae) {
+			ae.printStackTrace();
+			System.out.println("예외메시지 : "+ae.getMessage());
+		}
+		System.out.println(6);
+	}
+
+}
