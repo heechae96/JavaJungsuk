e59b025cef374443839997d3da2fa6005197a39b
heechae96 <shinheechae@naver.com>
2022-06-02 00:35:50 +0900

    toString()

---
+++ javajungsuk/src/ch09/Ch9_5.java
@@ -0,0 +1,20 @@
+package ch09;
+
+import java.util.Date;
+
+public class Ch9_5 {
+
+	public static void main(String[] args) {
+
+		// toString()
+		
+		String str = new String("KOREA");
+		Date today = new Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		System.out.println(today);
+		System.out.println(today.toString());
+	}
+
+}
---
+++ javajungsuk/src/ch09/Ch9_6.java
@@ -0,0 +1,34 @@
+package ch09;
+
+class Card1{
+	String kind;
+	int number;
+	
+	Card1(){
+		this("SPADE", 1);
+	}
+	
+	Card1(String kind, int number){
+		this.kind = kind;
+		this.number = number;
+	}
+	
+	public String toString() {
+		return "kind : " + kind + ", number : " + number;
+	}
+}
+
+public class Ch9_6 {
+
+	public static void main(String[] args) {
+
+		// toString()
+		
+		Card1 c1 = new Card1();
+		Card1 c2 = new Card1("HEART", 10);
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+		
+	}
+
+}
