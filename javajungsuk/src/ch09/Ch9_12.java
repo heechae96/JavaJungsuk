5948813d5dd2b26169d55940fa69fba323e93e8a
heechae96 <shinheechae@naver.com>
2022-06-02 23:04:32 +0900

    문자열 리터럴

---
+++ javajungsuk/src/ch09/Ch9_12.java
@@ -0,0 +1,16 @@
+package ch09;
+
+public class Ch9_12 {
+
+	public static void main(String[] args) {
+
+		// 문자열 리터럴
+		
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+		
+	}
+
+}
