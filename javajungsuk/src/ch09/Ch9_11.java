9b7d628f8ba019b9c17e822e0fc7d5f9671e227e
heechae96 <shinheechae@naver.com>
2022-06-02 22:51:07 +0900

    문자열의 비교

---
+++ javajungsuk/src/ch09/Ch9_11.java
@@ -0,0 +1,28 @@
+package ch09;
+
+public class Ch9_11 {
+
+	public static void main(String[] args) {
+
+		// 문자열의 비교
+		
+		String str1 = "abc";
+		String str2 = "abc";
+		System.out.println("String str1 = \"abc\";");
+		System.out.println("String str2 = \"abc\";");
+		
+		System.out.println("str1 == str2 ? " + (str1 == str2));
+		System.out.println("str1.equals(str2) ? " + str1.equals(str2));
+		System.out.println();
+		
+		String str3 = new String("\"abc\"");
+		String str4 = new String("\"abc\"");
+		
+		System.out.println("String str3 = \"abc\";");
+		System.out.println("String str4 = \"abc\";");
+		
+		System.out.println("str3 == str4 ? " + (str3 == str4));
+		System.out.println("str3.equals(str4) ? " + str3.equals(str4));
+	}
+
+}
