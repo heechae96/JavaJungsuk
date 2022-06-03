e301c49a5570ec2b1c14e79533822aff93925903
heechae96 <shinheechae@naver.com>
2022-06-04 01:02:11 +0900

    StringBuffer클래스의 생성자와 메서드

---
+++ javajungsuk/src/ch09/Ch9_19.java
@@ -0,0 +1,31 @@
+package ch09;
+
+public class Ch9_19 {
+
+	public static void main(String[] args) {
+
+		// StringBuffer클래스의 생성자와 메서드
+		
+		StringBuffer sb = new StringBuffer("01");	// capacity = 기존 16 + 2
+		StringBuffer sb2 = sb.append(23);
+		sb.append('4').append(56);
+		
+		StringBuffer sb3 = sb.append(78);
+		sb3.append(9.0);
+		
+		System.out.println("sb = " + sb);
+		System.out.println("sb2 = " + sb2);
+		System.out.println("sb3 = " + sb3);
+		
+		System.out.println("sb = " + sb.deleteCharAt(10));
+		System.out.println("sb = " + sb.delete(3, 6));
+		System.out.println("sb = " + sb.insert(3, "abc"));
+		System.out.println("sb = " + sb.replace(6, sb.length(), "END"));
+		
+		System.out.println("capacity = " + sb.capacity());
+		System.out.println("length = " + sb.length());
+		
+		
+	}
+
+}
