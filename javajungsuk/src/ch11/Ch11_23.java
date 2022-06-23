43180533c9e7b19f3223593e92a818e0384c61e1
heechae96 <shinheechae@naver.com>
2022-06-24 01:29:26 +0900

    HashSet

---
+++ javajungsuk/src/ch11/Ch11_23.java
@@ -0,0 +1,36 @@
+package ch11;
+
+import java.util.HashSet;
+
+public class Ch11_23 {
+
+	public static void main(String[] args) {
+
+		// HashSet
+		
+		HashSet set = new HashSet();
+		
+		set.add("abc");
+		set.add("abc");
+		set.add(new Person("David", 10));
+		set.add(new Person("David", 10));
+		
+		System.out.println(set);
+		
+	}
+
+}
+
+class Person{
+	String name;
+	int age;
+	
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String toString() {
+		return name + " : " + age;
+	}
+}
