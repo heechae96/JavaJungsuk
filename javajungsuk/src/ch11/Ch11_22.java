11fbd54a22b30f2c61f13341b64e6021d88f1205
heechae96 <shinheechae@naver.com>
2022-06-23 22:55:14 +0900

    LinkedHashSet

---
+++ javajungsuk/src/ch11/Ch11_22.java
@@ -0,0 +1,31 @@
+package ch11;
+
+import java.util.*;
+
+public class Ch11_22 {
+
+	public static void main(String[] args) {
+
+		// LinkedHashSet
+
+//		Set set = new HashSet();
+		Set set = new LinkedHashSet();
+		int[][] board = new int[5][5];
+		
+		for(int i=0; set.size()<25; i++) {
+			set.add((int)(Math.random()*50)+1+"");
+		}
+		
+		Iterator it = set.iterator();
+		
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length; j++) {
+				board[i][j] = Integer.parseInt((String)it.next());
+				System.out.print((board[i][j] < 10 ? "  " : " ") 
+														+ board[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
