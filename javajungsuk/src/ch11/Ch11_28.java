77622ec3b3a36590f88101cf155c6d0623e8a041
heechae96 <shinheechae@naver.com>
2022-06-24 21:05:23 +0900

    TreeSet

---
+++ javajungsuk/src/ch11/Ch11_28.java
@@ -0,0 +1,16 @@
+package ch11;
+
+public class Ch11_28 {
+
+	public static void main(String[] args) {
+
+		// TreeSet
+		
+		char ch = ' '; // 유니코드 기준 20번
+		// 공백(' ')이후의 문자들을 출력
+		for(int i=0; i<95; i++)
+			System.out.print(ch++);
+		
+	}
+
+}
