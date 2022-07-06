ce81517989525b7346238851a28ad9a986e2c49b
heechae96 <shinheechae@naver.com>
2022-07-06 23:58:21 +0900

    interrupt()와 interrupted()

---
+++ javajungsuk/src/ch13/Ch13_14.java
@@ -0,0 +1,36 @@
+package ch13;
+
+import javax.swing.JOptionPane;
+
+public class Ch13_14 {
+
+	public static void main(String[] args) {
+		
+		// interrupt()와 interrupted()
+		
+		ThreadEx14_1 th1 = new ThreadEx14_1();
+		th1.start();
+		
+		String input = JOptionPane.showInputDialog("아무값이나 입력하세요");
+		System.out.println("입력하신 값은 " + input + "입니다");
+		th1.interrupt();	// interrupt()를 호출하면, interrupted가 true가 된다
+		System.out.println("isInterrupted(): " + th1.isInterrupted());
+		
+	}
+}
+
+class ThreadEx14_1 extends Thread{
+	public void run() {
+		int i = 10;
+		
+		while(i != 0 && !isInterrupted()) {
+			System.out.println(i--);
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e) {
+//				interrupt();
+			}
+		}
+		System.out.println("카운트가 종료되었습니다");
+	}
+}
