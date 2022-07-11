db6940f8afc8285b3e723aed85992c88710680dc
heechae96 <shinheechae@naver.com>
2022-07-12 00:01:44 +0900

    함수형 인터페이스 타입의 매개변수와 반환타입

---
+++ javajungsuk/src/ch14/Ch14_1.java
@@ -0,0 +1,46 @@
+package ch14;
+
+@FunctionalInterface
+interface MyFunction{
+	void run();		// public abstract void run();
+}
+
+// 함수형 인터페이스 타입의 매개변수와 반환타입
+public class Ch14_1 {
+
+	static void execute(MyFunction f) {
+		// 매개변수의 타입이 MyFunction인 메서드
+		f.run();
+	}
+	
+	static MyFunction getMyFunction() {
+		// 반환타입이 MyFunction인 메서드
+		MyFunction f = () -> System.out.println("f3.run()");
+		return f;
+	}
+	
+	public static void main(String[] args) {
+				
+		// 람다식으로 MyFunction의 run()을 구현
+		MyFunction f1 = () -> System.out.println("f1.run()");
+		
+		MyFunction f2 = new MyFunction() {
+			// 익명클래스로 run()을 구현
+			@Override
+			public void run() {
+				System.out.println("f2.run()");
+			}
+		};
+		
+		MyFunction f3 = getMyFunction();
+		
+		f1.run();
+		f2.run();
+		f3.run();
+		
+		execute(f1);
+		execute(() -> System.out.println("run()"));
+		
+		
+	}
+}
