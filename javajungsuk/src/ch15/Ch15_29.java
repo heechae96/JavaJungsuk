2826961da461e42417a4a9ef6f5a9ca5c95da970
heechae96 <shinheechae@naver.com>
2022-07-26 01:01:23 +0900

    File

---
+++ javajungsuk/src/ch15/Ch15_29.java
@@ -0,0 +1,32 @@
+ package ch15;
+
+import java.io.File;
+
+public class Ch15_29 {
+
+	public static void main(String[] args) {
+
+		// File
+		
+		if(args.length != 1) {
+			System.out.println("USAGE : java Ch15_29 DIRECTORY");
+			System.exit(0);
+		}
+		
+		File f = new File(args[0]);
+		
+		if(!f.exists() || !f.isDirectory()) {
+			System.out.println("유효하지 않은 디렉토리입니다");
+			System.exit(0);
+		}
+		
+		File[] files = f.listFiles();
+		
+		for(int i=0; i<files.length; i++) {
+			String fileName = files[i].getName();
+			System.out.println(files[i].isDirectory() ? 
+											"["+fileName+"]" : fileName);
+		}
+	}
+
+}
---
+++ javajungsuk/src/ch15/Ch15_31.java
@@ -0,0 +1,41 @@
+package ch15;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ch15_31 {
+
+	public static void main(String[] args) {
+
+		// File
+		
+		String currDir = System.getProperty("user.dir");
+		File dir = new File(currDir);
+		
+//		System.out.println(dir + "   test!!!");
+		
+		File[] files = dir.listFiles();
+		
+		for(int i=0; i<files.length; i++) {
+			File f = files[i];
+			String name = f.getName();
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mma");
+			String attribute = "";
+			String size = "";
+			
+			if(files[i].isDirectory()) {
+				attribute = "DIR";
+			}else {
+				size = f.length() + "";
+				attribute = f.canRead() ? "R" : " ";
+				attribute += f.canWrite() ? "W" : " ";
+				attribute += f.isHidden() ? "H" : " ";
+			}
+			
+			System.out.printf("%s %3s %6s %s\n",
+					df.format(new Date(f.lastModified())), attribute, size, name);
+		}
+	}
+
+}
