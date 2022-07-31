6cc56712c161c51daaa9fe65547ea07975270c65
heechae96 <shinheechae@naver.com>
2022-08-01 01:35:16 +0900

    TCP소켓 프로그래밍

---
+++ javajungsuk/src/ch16/Ch16_12.java
@@ -0,0 +1,33 @@
+package ch16;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class Ch16_12 {
+
+	public static void main(String[] args) {
+
+		// TCP소켓 프로그래밍
+		
+		try {
+			String serverIp = "127.0.0.1";
+			// 소켓을 생성하여 연결을 요청한다
+			Socket socket = new Socket(serverIp, 7777);
+			
+			System.out.println("서버에 연결되었습니다");
+			Sender sender = new Sender(socket);
+			Receiver receiver = new Receiver(socket);
+			
+			sender.start();
+			receiver.start();
+		}catch(ConnectException ce) {
+			ce.printStackTrace();
+		}catch(IOException ie) {
+			ie.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
