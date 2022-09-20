828dccdde3e6f9afc13c2e87031169ab4682e8e8
heechae96 <shinheechae@naver.com>
2022-09-21 01:01:44 +0900

    변수 연습문제(1,2,6,7,9)

---
+++ src/ch3/Ch3_4.java
@@ -0,0 +1,9 @@
+package ch3;
+
+public class Ch3_4 {
+    public static void main(String[] args) {
+        // 456 -> 400 , 111 -> 100
+        int num = 111;
+        System.out.println(num / 100 * 100);
+    }
+}
---
+++ src/ch3/Ch3_5.java
@@ -0,0 +1,9 @@
+package ch3;
+
+public class Ch3_5 {
+    public static void main(String[] args) {
+        // 333 -> 331 , 777 -> 771
+        int num = 777;
+        System.out.println(num / 10 * 10 + 1);
+    }
+}
