1f79ebf4569bb7987373cb8e90d2ead630db26cd
heechae96 <shinheechae@naver.com>
2022-09-21 23:50:00 +0900

    다시 풀기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("1단원: 1,2,6,7,9");
+        System.out.println("2단원: 1,3,4,5,6,8,9,10,11,12,13,15");
     }
 }
