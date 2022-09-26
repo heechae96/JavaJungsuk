e6d70f9dd548b169cb0fc76d97b2596c4485c52a
heechae96 <shinheechae@naver.com>
2022-09-26 20:29:59 +0900

    다시 풀기

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("1단원: 1,2,6,7,9");
         System.out.println("2단원: 1,3,4,5,6,8,9,10,11,12,13,15");
         System.out.println("3단원: 1,5,8,9,");
-
+        System.out.println("4단원: 7,8,9,10,11,14,16,17,18,19,20,22,23,");
     }
 }
